5c26c8c265edfbf76cb48c2227a1600c555841c7
Brendan <46061381+AtlasDisease@users.noreply.github.com>
2024-03-20 16:07:11 -0500

    Add files via upload

--- uMainForm.dfm
+++ uMainForm.dfm
@@ -1,8 +1,8 @@
 object Form1: TForm1
-  Left = 1218
-  Top = 175
-  Width = 676
-  Height = 361
+  Left = 1077
+  Top = 305
+  Width = 704
+  Height = 351
   Caption = 'QD Test Stuff'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -13,15 +13,365 @@ object Form1: TForm1
   OldCreateOrder = False
   OnCreate = FormCreate
   OnDestroy = FormDestroy
+  DesignSize = (
+    696
+    317)
   PixelsPerInch = 96
   TextHeight = 13
+  object Panel5: TPanel
+    Left = 88
+    Top = 8
+    Width = 601
+    Height = 297
+    TabOrder = 9
+    object GroupBox1: TGroupBox
+      Left = 8
+      Top = 8
+      Width = 393
+      Height = 121
+      Caption = 'Address'
+      TabOrder = 0
+      object Label5: TLabel
+        Left = 8
+        Top = 24
+        Width = 81
+        Height = 13
+        Caption = 'Address Number:'
+      end
+      object Label6: TLabel
+        Left = 16
+        Top = 56
+        Width = 72
+        Height = 13
+        Caption = 'Address Name:'
+      end
+      object Label12: TLabel
+        Left = 24
+        Top = 88
+        Width = 59
+        Height = 13
+        Caption = 'Address Zip:'
+      end
+      object edtAddrNum: TEdit
+        Left = 96
+        Top = 24
+        Width = 73
+        Height = 21
+        TabOrder = 0
+      end
+      object edtAddrName: TEdit
+        Left = 96
+        Top = 56
+        Width = 217
+        Height = 21
+        TabOrder = 1
+      end
+      object edtAptNum: TEdit
+        Left = 320
+        Top = 56
+        Width = 65
+        Height = 21
+        TabOrder = 2
+      end
+      object edtAddrZip: TEdit
+        Left = 96
+        Top = 88
+        Width = 121
+        Height = 21
+        TabOrder = 3
+      end
+    end
+    object btnCreateAddress: TButton
+      Left = 8
+      Top = 136
+      Width = 89
+      Height = 25
+      Caption = 'Create Address'
+      TabOrder = 1
+      OnClick = btnCreateAddressClick
+    end
+  end
+  object Panel4: TPanel
+    Left = 88
+    Top = 8
+    Width = 601
+    Height = 297
+    TabOrder = 8
+    object Label4: TLabel
+      Left = 24
+      Top = 8
+      Width = 59
+      Height = 13
+      Caption = 'State Name:'
+    end
+    object Label3: TLabel
+      Left = 12
+      Top = 40
+      Width = 70
+      Height = 13
+      Caption = 'Country Name:'
+    end
+    object edtStateName: TEdit
+      Left = 88
+      Top = 8
+      Width = 121
+      Height = 21
+      TabOrder = 0
+    end
+    object edtCountryName: TEdit
+      Left = 88
+      Top = 40
+      Width = 321
+      Height = 21
+      TabOrder = 1
+    end
+    object btnCreateCountry: TButton
+      Left = 16
+      Top = 72
+      Width = 121
+      Height = 25
+      Caption = 'Create State/Country'
+      TabOrder = 2
+      OnClick = btnCreateCountryClick
+    end
+  end
+  object Panel3: TPanel
+    Left = 88
+    Top = 8
+    Width = 601
+    Height = 297
+    TabOrder = 5
+    object Label2: TLabel
+      Left = 8
+      Top = 8
+      Width = 67
+      Height = 13
+      Caption = 'County Name:'
+    end
+    object edtCountyName: TEdit
+      Left = 80
+      Top = 8
+      Width = 121
+      Height = 21
+      TabOrder = 0
+    end
+    object btnCreateCounty: TButton
+      Left = 208
+      Top = 8
+      Width = 81
+      Height = 25
+      Caption = 'Create County'
+      TabOrder = 1
+      OnClick = btnCreateCountyClick
+    end
+  end
+  object Panel2: TPanel
+    Left = 88
+    Top = 8
+    Width = 601
+    Height = 297
+    Enabled = False
+    TabOrder = 4
+    object Label1: TLabel
+      Left = 8
+      Top = 8
+      Width = 51
+      Height = 13
+      Caption = 'City Name:'
+    end
+    object edtCityName: TEdit
+      Left = 64
+      Top = 8
+      Width = 121
+      Height = 21
+      TabOrder = 0
+    end
+    object rgCityTypes: TRadioGroup
+      Left = 8
+      Top = 32
+      Width = 177
+      Height = 153
+      Caption = 'City Types'
+      ItemIndex = 0
+      Items.Strings = (
+        'Lost'
+        'Site'
+        'Community'
+        'Town'
+        'City')
+      TabOrder = 1
+      OnClick = rgCityTypesClick
+    end
+    object rgAdminTypes: TRadioGroup
+      Left = 8
+      Top = 192
+      Width = 177
+      Height = 97
+      Caption = 'Administrative Types'
+      ItemIndex = 0
+      Items.Strings = (
+        'None'
+        'Seat'
+        'Capital')
+      TabOrder = 2
+      OnClick = rgAdminTypesClick
+    end
+    object btnCreateCity: TButton
+      Left = 192
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = 'Create City'
+      TabOrder = 3
+      OnClick = btnCreateCityClick
+    end
+  end
+  object Panel1: TPanel
+    Left = 88
+    Top = 8
+    Width = 601
+    Height = 297
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    TabOrder = 1
+    DesignSize = (
+      601
+      297)
+    object Label7: TLabel
+      Left = 24
+      Top = 8
+      Width = 20
+      Height = 13
+      Caption = 'City:'
+    end
+    object Label8: TLabel
+      Left = 8
+      Top = 32
+      Width = 36
+      Height = 13
+      Caption = 'County:'
+    end
+    object Label9: TLabel
+      Left = 16
+      Top = 56
+      Width = 28
+      Height = 13
+      Caption = 'State:'
+    end
+    object Label10: TLabel
+      Left = 8
+      Top = 80
+      Width = 39
+      Height = 13
+      Caption = 'Country:'
+    end
+    object Label11: TLabel
+      Left = 8
+      Top = 120
+      Width = 41
+      Height = 13
+      Caption = 'Address:'
+    end
+    object cbCities: TComboBox
+      Left = 56
+      Top = 8
+      Width = 145
+      Height = 21
+      ItemHeight = 13
+      TabOrder = 0
+    end
+    object cbCounties: TComboBox
+      Left = 56
+      Top = 32
+      Width = 145
+      Height = 21
+      ItemHeight = 13
+      TabOrder = 1
+    end
+    object cbStates: TComboBox
+      Left = 56
+      Top = 56
+      Width = 145
+      Height = 21
+      ItemHeight = 13
+      TabOrder = 2
+    end
+    object cbCountries: TComboBox
+      Left = 56
+      Top = 80
+      Width = 145
+      Height = 21
+      ItemHeight = 13
+      TabOrder = 3
+    end
+    object cbAddresses: TComboBox
+      Left = 56
+      Top = 120
+      Width = 145
+      Height = 21
+      ItemHeight = 13
+      TabOrder = 4
+    end
+    object memo1: TMemo
+      Left = 208
+      Top = 8
+      Width = 385
+      Height = 281
+      Anchors = [akLeft, akTop, akRight, akBottom]
+      TabOrder = 5
+    end
+  end
   object btnShow: TButton
     Left = 8
-    Top = 16
+    Top = 280
     Width = 75
     Height = 25
     Caption = 'Show'
     TabOrder = 0
     OnClick = btnShowClick
+  end
+  object btnCreate: TButton
+    Left = 8
+    Top = 248
+    Width = 75
+    Height = 25
+    Caption = 'Create'
+    TabOrder = 2
+    OnClick = btnCreateClick
+  end
+  object btnAddCity: TButton
+    Left = 8
+    Top = 8
+    Width = 75
+    Height = 25
+    Caption = 'Add City'
+    TabOrder = 3
+    OnClick = btnAddCityClick
+  end
+  object btnAddCounty: TButton
+    Left = 8
+    Top = 40
+    Width = 75
+    Height = 25
+    Caption = 'Add County'
+    TabOrder = 6
+    OnClick = btnAddCountyClick
+  end
+  object btnAddCountry: TButton
+    Left = 8
+    Top = 72
+    Width = 75
+    Height = 25
+    Caption = 'Add Country'
+    TabOrder = 7
+    OnClick = btnAddCountryClick
+  end
+  object btnAddAddress: TButton
+    Left = 8
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Add Address'
+    TabOrder = 10
+    OnClick = btnAddAddressClick
   end
 end
--- uMainForm.pas
+++ uMainForm.pas
@@ -4,21 +4,75 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls; {FileCtrl, Registry, IniFiles, DB, dbisamtb, entryprog,
+  Dialogs, StdCtrls, ExtCtrls; {FileCtrl, Registry, IniFiles, DB, dbisamtb, entryprog,
   Grids, DBGrids, ClientServer, objDBDataItem, TypeDecsDB, objDBData;}
 
 type
   TForm1 = class(TForm)
     btnShow: TButton;
+    Panel1: TPanel;
+    btnCreate: TButton;
+    btnAddCity: TButton;
+    Panel2: TPanel;
+    Label1: TLabel;
+    edtCityName: TEdit;
+    rgCityTypes: TRadioGroup;
+    rgAdminTypes: TRadioGroup;
+    btnCreateCity: TButton;
+    Panel3: TPanel;
+    Label2: TLabel;
+    edtCountyName: TEdit;
+    btnCreateCounty: TButton;
+    btnAddCounty: TButton;
+    btnAddCountry: TButton;
+    Panel4: TPanel;
+    Label4: TLabel;
+    edtStateName: TEdit;
+    Label3: TLabel;
+    edtCountryName: TEdit;
+    btnCreateCountry: TButton;
+    Panel5: TPanel;
+    GroupBox1: TGroupBox;
+    Label5: TLabel;
+    Label6: TLabel;
+    edtAddrNum: TEdit;
+    edtAddrName: TEdit;
+    edtAptNum: TEdit;
+    btnAddAddress: TButton;
+    btnCreateAddress: TButton;
+    cbCities: TComboBox;
+    Label7: TLabel;
+    cbCounties: TComboBox;
+    Label8: TLabel;
+    cbStates: TComboBox;
+    Label9: TLabel;
+    cbCountries: TComboBox;
+    Label10: TLabel;
+    cbAddresses: TComboBox;
+    Label11: TLabel;
+    Label12: TLabel;
+    edtAddrZip: TEdit;
+    memo1: TMemo;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure btnShowClick(Sender: TObject);
+    procedure rgCityTypesClick(Sender: TObject);
+    procedure rgAdminTypesClick(Sender: TObject);
+    procedure btnAddCityClick(Sender: TObject);
+    procedure btnCreateCityClick(Sender: TObject);
+    procedure btnAddCountyClick(Sender: TObject);
+    procedure btnCreateCountyClick(Sender: TObject);
+    procedure btnCreateCountryClick(Sender: TObject);
+    procedure btnAddCountryClick(Sender: TObject);
+    procedure btnAddAddressClick(Sender: TObject);
+    procedure btnCreateAddressClick(Sender: TObject);
+    procedure btnCreateClick(Sender: TObject);
   private
     //ADBData: TDBData;
   public
   end;
 
-  CityTypes = (Lost, Site, Community, Town, City);
+CityTypes = (Lost, Site, Community, Town, City);
   AdministrativeTypes = (None, Seat, Capital);
 
   TCity = packed class(TObject)
@@ -75,22 +129,6 @@ type
         read getDesc;}
     end;
 
-  TAddress = packed class(TObject)
-    protected
-      addr1: TAddressName;
-      addr2: TAddressName;
-      city: TCity;
-      state: TState;
-      zip: String;
-    private
-      function getDesc: String;
-    public
-      constructor Create(addr1, addr2: TAddressName; city: TCity; state: TState; zip: string); overload;
-    published
-      property desc: String
-        read getDesc;
-  end;
-
   TLocation = packed class(TObject)
     protected
       address: TAddressName;
@@ -107,15 +145,34 @@ type
         read getDesc;
   end;
 
+  TAddress = packed class(TObject)
+    protected
+      addr1: TAddressName;
+      addr2: TAddressName;
+      city: TCity;
+      state: TState;
+      country: TCountry;
+      zip: String;
+    private
+      function getDesc: String;
+    public
+      constructor Create(addr1, addr2: TAddressName; city: TCity; state: TState; country: TCountry; zip: string); overload;
+      constructor Create(location: TLocation; zip: string); overload;
+    published
+      property desc: String
+        read getDesc;
+  end;
+
+  function formatAsMailAddress(recipient: String; address: TAddress): String;
+  function formatAsAddress(address: TAddress): String;
+
 var
   Form1: TForm1;
-  address_name: TAddressName;
   address: TAddress;
-  myCity: TCity;
-  county: TCounty;
-  state: TState;
-  country: TCountry;
   location: TLocation;
+  citytype: CityTypes;
+  admintype: AdministrativeTypes;
+  cities, counties, states, countries, addresses: TList;
 
 implementation
 
@@ -149,21 +206,21 @@ end;
 
 function TCounty.getDesc: String;
 begin
-Result := self.name + ' County';
+  Result := self.name + ' County';
 end;
 
 constructor TState.Create(name, abbv: String);
 begin
-self.name := name;
-self.subdivisions := TList.Create;
-self.abbv := abbv;
+  self.name := name;
+  self.subdivisions := TList.Create;
+  self.abbv := abbv;
 end;
 
 constructor TState.Create(name, abbv: String; counties: TList);
 begin
-self.name := name;
-self.subdivisions := counties;
-self.abbv := abbv;
+  self.name := name;
+  self.subdivisions := counties;
+  self.abbv := abbv;
 end;
 
 procedure TState.addCounty(new_county: TCounty);
@@ -194,21 +251,42 @@ begin
 Result := trim(inttostr(self.number) + ' ' + self.name);
 end;}
 
-constructor TAddress.Create(addr1, addr2: TAddressName; city: TCity; state: TState; zip: string);
+constructor TAddress.Create(addr1, addr2: TAddressName; city: TCity; state: TState; country: TCountry; zip: string);
 begin
   self.addr1 := addr1;
   self.addr2 := addr2;
   self.city := city;
   self.state := state;
+  self.country := country;
+  self.zip := zip;
+end;
+
+constructor TAddress.Create(location: TLocation; zip: string);
+begin
+  self.addr1 := location.address;
+  self.addr2 := TAddressName.Create;
+  self.city := location.city;
+  self.state := location.state;
+  self.country := location.country;
   self.zip := zip;
 end;
 
 function TAddress.getDesc: String;
 begin
-Result := self.addr1.name;
-if (self.addr2 <> nil) then
-  Result := Result + trim(' ' + self.addr2.name);
-Result := Result + ', ' + self.city.name + ', ' + self.state.name + ' ' + self.zip;
+  Result := inttostr(self.addr1.number) + ' ' + self.addr1.name;
+  if (self.addr2 <> nil) then
+    Result := Result + trim(' ' + self.addr2.name);
+  Result := Result + ', ' + self.city.name + ', ';
+  if (self.state <> nil) then
+    begin
+    if (self.state.name <> '') then
+      Result := Result + self.state.name
+    else
+      Result := Result + self.country.name;
+    end
+  else
+    Result := Result + self.country.name;
+  Result := trim(Result + ' ' + self.zip);
 end;
 
 constructor TLocation.Create(address: TAddressName; city: TCity; county: TCounty; state: TState; country: TCountry);
@@ -220,68 +298,245 @@ begin
   self.country := country;
 end;
 
+{constructor TLocation.Create(address: TAddress);
+begin
+  self.address := address.addr1;
+  self.city := address.city;
+  self.county := address.county;
+  self.state := address.state;
+  self.country := address.country;
+end;}
+
 function TLocation.getDesc: String;
 begin
-Result := trim(self.address.name + ', ' + self.city.name + ', ' + self.county.desc + ', ' + self.state.name + ', ' + self.country.name);
+Result := inttostr(self.address.number) + ' ' + self.address.name + ', ' + self.city.name + ', ' + self.county.desc;
+if (self.state <> nil) then
+  begin
+  if (self.state.name <> '') then
+    Result := Result + ', ' + self.state.name;
+  end;
+Result := trim(Result + ', ' + self.country.name);
+end;
+
+function formatAsMailAddress(recipient: String; address: TAddress): String;
+begin
+  Result := recipient + AnsiString(#13#10);
+  Result := Result + inttostr(address.addr1.number) + ' ' + address.addr1.name + AnsiString(#13#10);
+
+  Result := Result + address.city.name;
+  if (address.state <> nil) then
+    begin
+    if (address.state.name <> '') then
+      Result := Result + ', ' + address.state.name;
+    end;
+    
+  Result := Result + ' ' + address.zip + AnsiString(#13#10);
+  Result := Result + address.country.name;
+end;
+
+function formatAsAddress(address: TAddress): String;
+begin
+  Result := inttostr(address.addr1.number) + ' ' + address.addr1.name + ', ' + address.city.name + ', ';
+  if (address.state <> nil) then
+    begin
+    if (address.state.name <> '') then
+      Result := Result + address.state.name + ', ';
+    end;
+
+  Result := Result + address.country.name + ' ' + address.zip;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-cities, counties, states: TList;
 begin
 {ADBData := TDBData.Create;
 CreateClientServerObj(ADBData,dmMod1);
 
 tblAccountGroupsAccounts := ADBData.AddATable('tblAccountGroupsAccounts',ttAccountGroupsAccounts,dbdiTable,false,dbdiQuery);}
 
-mycity := TCity.Create('College Station', City, None);
-
 cities := TList.Create;
-cities.add(mycity);
-
-county := TCounty.Create('Brazos', cities);
-
 counties := TList.Create;
-counties.add(county);
-
-state := TState.Create('Texas', 'TX', counties);
-
 states := TList.Create;
-states.add(state);
+countries := TList.Create;
+addresses := TList.Create;
 
-country := TCountry.Create('United States of America', 'U.S.A', states);
-
-address_name := TAddressName.Create(2701, 'Longmire Dr. #1405');
-
-address := TAddress.Create;
-address.addr1 := address_name;
-address.city := mycity;
-address.state := state;
-address.zip := '77845';
-
-location := TLocation.Create;
-location.address := address_name;
-location.city := mycity;
-location.county := county;
-location.state := state;
-location.country := country;
+Panel1.BringToFront;
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
 //FreeAndNil(ADBData);
-FreeAndNil(address);
-FreeAndNil(mycity);
-FreeAndNil(county);
-FreeAndNil(country);
-FreeAndNil(state);
-FreeAndNil(location);
+  FreeAndNil(address);
+//  FreeAndNil(mycity);
+//  FreeAndNil(county);
+//  FreeAndNil(country);
+//  FreeAndNil(state);
+  FreeAndNil(location);
+//  FreeAndNil(address_name);
 end;
 
 procedure TForm1.btnShowClick(Sender: TObject);
 begin
-ShowMessage(address.desc);
-ShowMessage(location.desc);
+  Panel1.BringToFront;
+  
+  memo1.Lines.Clear;
+  memo1.Lines.Append(address.desc);
+  memo1.Lines.Append('');
+  memo1.Lines.Append(location.desc);
+  memo1.Lines.Append('');
+  memo1.Lines.Append(formatAsMailAddress('Brendan Beard', address));
+  memo1.Lines.Append('');
+  memo1.Lines.Append(formatAsAddress(address));
+end;
+
+procedure TForm1.rgCityTypesClick(Sender: TObject);
+begin
+  case rgCityTypes.ItemIndex of
+    0: citytype := Lost;
+    1: citytype := Site;
+    2: citytype := Community;
+    3: citytype := Town;
+    4: citytype := City;
+  end;
+end;
+
+procedure TForm1.rgAdminTypesClick(Sender: TObject);
+begin
+  case rgAdminTypes.ItemIndex of
+    0: admintype := None;
+    1: admintype := Seat;
+    2: admintype := Capital;
+  end;
+end;
+
+procedure TForm1.btnAddCityClick(Sender: TObject);
+begin
+  Panel2.Enabled := true;
+  Panel2.BringToFront;
+end;
+
+procedure TForm1.btnCreateCityClick(Sender: TObject);
+var
+  mycity: TCity;
+begin
+  mycity := TCity.Create(edtCityName.Text, citytype, admintype);
+  cities.add(mycity);
+  cbCities.Items.Append(mycity.name);
+
+  // Clear
+  edtCityName.Text := '';
+  rgCityTypes.ItemIndex := 0;
+  rgAdminTypes.ItemIndex := 0;
+
+  Panel2.Enabled := false;
+  Panel2.SendToBack;
+end;
+
+procedure TForm1.btnAddCountyClick(Sender: TObject);
+begin
+  Panel3.Enabled := true;
+  Panel3.BringToFront;
+end;
+
+procedure TForm1.btnCreateCountyClick(Sender: TObject);
+var
+  county: TCounty;
+begin
+  county := TCounty.Create(edtCountyName.Text, cities);
+  counties.add(county);
+  cbCounties.Items.Append(county.name);
+
+  //Clear
+  edtCountyName.Text := '';
+
+  Panel3.Enabled := false;
+  Panel3.SendToBack;
+end;
+
+procedure TForm1.btnCreateCountryClick(Sender: TObject);
+var
+  state: TState;
+  country: TCountry;
+begin
+  if (edtStateName.Text <> '') then
+    begin
+    state := TState.Create(edtStateName.Text, '', counties);
+    states.add(state);
+    cbStates.Items.Append(state.name);
+
+    edtStateName.Text := '';
+    end;
+
+  if (edtCountryName.Text <> '') then
+    begin
+    country := TCountry.Create(edtCountryName.Text, '', states);
+    countries.Add(country);
+    cbCountries.Items.Append(country.name);
+
+    edtCountryName.Text := '';
+    end;
+
+  Panel4.Enabled := false;
+  Panel4.SendToBack;
+end;
+
+procedure TForm1.btnAddCountryClick(Sender: TObject);
+begin
+  Panel4.Enabled := true;
+  Panel4.BringToFront;
+end;
+
+procedure TForm1.btnAddAddressClick(Sender: TObject);
+begin
+Panel5.Enabled := true;
+Panel5.BringToFront;
+end;
+
+procedure TForm1.btnCreateAddressClick(Sender: TObject);
+var
+  apt_num, addrName: String;
+  address_name, address_name2: TAddressName;
+  city2: TCity;
+  state: TState;
+  country: TCountry;
+
+begin
+  apt_num := '';
+  if (edtAptNum.Text <> '') then
+    apt_num := '#' + edtAptNum.Text;
+
+  addrName := trim(edtAddrName.Text + ' ' + apt_num);
+  address_name := TAddressName.Create(strtoint(edtAddrNum.Text), addrName);
+  address_name2 := TAddressName.Create;
+
+  city2 := TCity.Create;
+  state := TState.Create;
+  country := TCountry.Create;
+
+  address := TAddress.Create(address_name, address_name2, city2, state, country, edtAddrZip.Text);
+  addresses.Add(address);
+  cbAddresses.Items.Append(address.addr1.name);
+
+  //Clear
+  edtAddrName.Text := '';
+  edtAddrNum.Text := '';
+  edtAptNum.Text := '';
+  edtAddrZip.Text := '';
+
+  Panel5.Enabled := false;
+  Panel5.SendToBack;
+end;
+
+procedure TForm1.btnCreateClick(Sender: TObject);
+begin
+location := TLocation.Create;
+location.address := TAddress(addresses.Items[cbAddresses.ItemIndex]).addr1;
+location.city := TCity(cities.Items[cbCities.ItemIndex]);
+location.county := TCounty(counties.Items[cbCounties.ItemIndex]);
+if (cbStates.ItemIndex <> -1) then
+  location.state := TState(states.Items[cbStates.ItemIndex]);
+location.country := TCountry(countries.Items[cbCountries.ItemIndex]);
+
+address := TAddress.Create(location, '77845');
 end;
 
 end.
